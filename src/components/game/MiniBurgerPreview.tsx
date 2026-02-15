b80955be0861e432d1d58a6d06de5df437952f77
girgir <wjdrlf5986@naver.com>
2026-02-15 13:36:06 +0900

    상대방 미니 버거 미리보기 컴포넌트 개선: 스케일 조정 및 래퍼 추가

--- src/components/game/MiniBurgerPreview.tsx
+++ src/components/game/MiniBurgerPreview.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
 import type { Ingredient } from '@/types';
 
 const INGREDIENT_IMAGES: Record<Ingredient, string> = {
@@ -16,16 +17,40 @@ interface MiniBurgerPreviewProps {
 }
 
 export default function MiniBurgerPreview({ ingredients }: MiniBurgerPreviewProps) {
+  const wrapRef = useRef<HTMLDivElement>(null);
+  const foodRef = useRef<HTMLDivElement>(null);
+
+  // 재료가 바뀔 때마다: 컨테이너 높이를 넘으면 너비를 줄여 비율 유지
+  useEffect(() => {
+    const wrap = wrapRef.current;
+    const food = foodRef.current;
+    if (!wrap || !food) return;
+
+    food.style.width = '100%';
+    food.style.margin = '';
+
+    const availableH = wrap.clientHeight;
+    const naturalH = food.scrollHeight;
+
+    if (naturalH > availableH && availableH > 0) {
+      const scale = availableH / naturalH;
+      food.style.width = `${scale * 100}%`;
+      food.style.margin = '0 auto';
+    }
+  }, [ingredients]);
+
   return (
-    <div className="mini-burger">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src="/ingredient/bun_bottom.png" alt="" className="mini-burger__bun" />
-      {ingredients.map((ing, i) => (
-        <div key={i} className={`mini-burger__ing mini-burger__ing--${ing}`}>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={INGREDIENT_IMAGES[ing]} alt={ing} />
-        </div>
-      ))}
+    <div className="mini-burger-wrap" ref={wrapRef}>
+      <div className="mini-burger" ref={foodRef}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/ingredient/bun_bottom.png" alt="" className="mini-burger__bun" />
+        {ingredients.map((ing, i) => (
+          <div key={i} className={`mini-burger__ing mini-burger__ing--${ing}`}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={INGREDIENT_IMAGES[ing]} alt={ing} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
